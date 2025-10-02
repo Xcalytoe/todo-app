377312fd082a0b15ce4446974d78d81f944bcfa1
Ezejiofor Cosmas <ezejioforcosmas@gmail.com>
2025-10-02 03:05:12 +0100

    chore: update query filter

--- src/controllers/todoControllers.js
+++ src/controllers/todoControllers.js
@@ -4,24 +4,44 @@ const Todo = require("../models/Todo");
 // group todo list by date
 const groupByDate = (todoArr) => {
   return todoArr.reduce((acc, todo) => {
-    const date = todo.date;
-    const completedAt = format(new Date(todo.completedAt), "PPpp");
-    const formattedDate = format(new Date(todo.date), "dd-MM-yyyy");
-
+    const dateKey = todo.date.toISOString().split("T")[0];
+    const completedAt = {
+      ...(todo?.completedAt && format(new Date(todo.completedAt), "PPpp")),
+    };
     // Check if the key exists
-    if (!acc[date]) {
-      acc[date] = [];
+    if (!acc[dateKey]) {
+      acc[dateKey] = [];
     }
-    acc[date].push({ ...todo, completedAt, date: formattedDate });
+
+    const todoObj = todo.toJSON();
+
+    // acc[dateKey].push({ ...todo, completedAt, date: formattedDate });
+    acc[dateKey].push({ ...todoObj, ...completedAt });
     return acc;
   }, {});
 };
 
 const getTodoList = async (req, res, next) => {
   try {
-    const todos = await Todo.find({ userId: req.user._id }).sort({ date: -1 });
+    const { title, date, completed } = req?.query || {};
+    let filter = {};
+    if (title) {
+      filter.title = { $regex: title, $options: "i" };
+    }
+    if (date) {
+      filter.date = date;
+    }
+    if (!!completed) {
+      filter.completed = completed === "true";
+    }
+
+    const todos = await Todo.find({ userId: req.user._id, ...filter }).sort({
+      date: 1,
+    });
 
     const list = groupByDate(todos);
+    // console.log("formattedDate", list, todos);
+
     res.render("todo/list", { todoList: list });
   } catch (error) {
     next(error);
